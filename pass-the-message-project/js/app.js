974157d7239412d74054f33b4733c940045688f6
jmedina0126 <j.medina0126@gmail.com>
2021-02-18 23:29:53 +0000

    First commit

---
+++ pass-the-message-project/js/app.js
@@ -0,0 +1,13 @@
+
+(function() {
+    // Get DOM
+    document.querySelector('#message');
+    document.querySelector('#SubmitBtn');
+    document.querySelector('.message-content');
+
+    // Events
+    
+})()
+
+
+
