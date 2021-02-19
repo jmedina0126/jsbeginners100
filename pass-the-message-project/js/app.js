2395c1137da147ca5b7507dbeb8b6d0ceb7f8219
Jemar <j.medina0126@gmail.com>
2021-02-19 10:07:08 +0000

    Cloned counter project

--- pass-the-message-project/js/app.js
+++ pass-the-message-project/js/app.js
@@ -1,13 +1,30 @@
 
 (function() {
     // Get DOM
-    document.querySelector('#message');
-    document.querySelector('#SubmitBtn');
-    document.querySelector('.message-content');
+    const form = document.querySelector('#message-form')
+    const message = document.querySelector('#message');
+    const btn = document.querySelector('#SubmitBtn');
+    const feedback = document.querySelector('.feedback')
+    const content = document.querySelector('.message-content');
 
     // Events
-    
-})()
+    form.addEventListener('submit', function(e) {
+
+        // Prevent forms default submission
+        e.preventDefault()
+
+        // 
+        if (message.value === '') {
+            feedback.classList.add('show')
+            setTimeout(function(){
+                feedback.classList.remove('show')
+            }, 2000)
+        } else {
+            content.textContent = message.value
+            message.value = ''
+        }
+    });
+})();
 
 
 
